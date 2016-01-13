7e1a478fb3fe03c131e4e4baaaa81db785fe1b02
Rene Martinez <renzp2@gmail.com>
2016-01-13 00:04:01 -0800

    Theme Commit

--- RehnzWebsite/RehnzWebsite/App_Start/BundleConfig.cs
+++ RehnzWebsite/RehnzWebsite/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace RehnzWebsite
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/FreeLancer.css"));
         }
     }
 }
