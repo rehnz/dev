bad59dbdebe19a65a3ce94e97512614dc9768c30
rehnz <renzp2@gmail.com>
2016-01-27 11:09:33 -0800

    Golf Homie App (Angular)

---
+++ GolfHomieWebApp/GolfHomieWebApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GolfHomieWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET HOME
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+
+        }
+
+        public JsonResult GetUsers()
+        {
+
+
+            string user = "Rene";
+
+            return Json(user,JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
---
+++ GolfHomieWebApp/GolfHomieWebApp/Scripts/App.js
@@ -0,0 +1,15 @@
+﻿
+var mainApp = angular.module('mainApp', []);
+
+mainApp.controller('homeController', function ($scope, $http)
+{
+    $http.get('Home/GetUsers/')
+        .success(function (result) 
+        {
+            $scope.user = result;
+        }
+        .error(function (data)
+        {
+            console.log(data)
+        })
+    )})
